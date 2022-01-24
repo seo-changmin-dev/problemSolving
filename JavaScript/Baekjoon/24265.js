ddcc76c2da8e67ec5dda55bafa4cd5a8552e70d3
scmDev <seo.changmin.dev@gmail.com>
2022-01-24 23:16:33 +0900

    [BOJ] 알고리즘 수업 - 알고리즘의 수행 시간 3-7
    7 같은 경우 인자를 미지수로 두고 시그마 3개를 풀면 된다.
    연속하는 세 개의 수는 항상 6의 배수이므로, int 변환은 필요 없다.

--- JavaScript/Baekjoon/24265.js
+++ JavaScript/Baekjoon/24265.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 fs.readFile('/dev/stdin', (err, input) => {
   const n = parseInt(input.toString());
 
-  console.log(n * n);
+  console.log(n * (n-1) / 2);
   console.log(2);
 });
